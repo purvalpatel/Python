6ddd3d86f0b46f07205abc6315e25fed593b1454
purval patel <patelpurval13@hotmail.com>
2016-03-29 14:20:37 +0530

    Update get_youtube_view.py

--- get_youtube_view.py
+++ get_youtube_view.py
@@ -1,13 +1,17 @@
+#last modified by : purval patel 
+#date : 29/03/2016
+
 import time
 import webbrowser
 
 #how much views you want
 
-totalBreaks = 30
-countBreaks = 0
+#totalBreaks = 30
+#countBreaks = 0
 
 print("Enjoy your Time\n" +time.ctime())
-while(countBreaks < totalBreaks):
+#while(countBreaks < totalBreaks):
     time.sleep(5) 
     webbrowser.open("https://www.youtube.com/watch?v=o6A7nf3IeeA")
-    countBreaks = countBreaks+1
+#    countBreaks = countBreaks+1
+#while loop runs number of times if it is implemented. so no need of while loop
